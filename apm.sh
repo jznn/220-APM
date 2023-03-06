5e8ebed8f67cca58b0ec370255c9c505bb7e8757
Justin Huang <59941045+jznn@users.noreply.github.com>
2023-03-06 16:18:01 -0500

    combined ps level with other functions

--- apm.sh
+++ apm.sh
@@ -4,41 +4,63 @@
 spawn(){
 #Passed in the IP as an argument
 	#spawn processes
-	sudo pwd	
-	sudo ./APM1 $1 & #& symbol allows for simultaneous 
+	pwd	
+	./APM1 $1 & #& symbol allows for simultaneous 
 	pd1=$!
-	echo "Started Process #1 with ID # : ${pd1}"
-	sudo ./APM2 $1 &
+	echo "Started Process #1 with ID #: ${pd1}"
+	./APM2 $1 &
 	pd2=$!
 	echo "Started Process #2 with ID #: ${pd2}"
-	sudo ./APM3 $1 &
+	./APM3 $1 &
 	pd3=$!
 	echo "Started Process #3 with ID #: ${pd3}"
-	sudo ./APM4 $1 &
+	./APM4 $1 &
 	pd4=$!
 	echo "Started Process #4 with ID #: ${pd4}"
-	sudo ./APM5 $1 &
+	./APM5 $1 &
 	pd5=$!
 	echo "Started Process #5 with ID #: ${pd5}"
-	sudo 
-./APM6 $1 &
+    ./APM6 $1 &
 	pd6=$!
 	echo "Started Process #6 with ID #: ${pd6}"
         echo "Started ifstat on the above processes"
 	ifstat -a -d 1	
 }
-cleanup(){
-#KILL THEM ALL ;)
-	kill -9 $pd1
-	kill -9 $pd2 
-	kill -9 $pd3
-	kill -9 $pd4
-	kill -9 $pd5
-	kill -9 $pd6
-	pkill -f -9 "ifstat"
-exit $?
+
+ps_level () {
+        cpu=$(ps -p "$pd1" -o %cpu | sed -n 2p)
+        mem=$(ps -p "$pd1" -o %mem | sed -n 2p)
+        echo $1,$cpu,$mem >> "APM1_metrics.csv"
+        cpu=$(ps -p "$pd2" -o %cpu | sed -n 2p)
+        mem=$(ps -p "$pd2" -o %mem | sed -n 2p)
+        echo $1,$cpu,$mem >> "APM2_metrics.csv"
+        cpu=$(ps -p "$pd3" -o %cpu | sed -n 2p)
+        mem=$(ps -p "$pd3" -o %mem | sed -n 2p)
+        echo $1,$cpu,$mem >> "APM3_metrics.csv"
+        cpu=$(ps -p "$pd4" -o %cpu | sed -n 2p)
+        mem=$(ps -p "$pd4" -o %mem | sed -n 2p)
+        echo $1,$cpu,$mem >> "APM4_metrics.csv"
+        cpu=$(ps -p "$pd5" -o %cpu | sed -n 2p)
+        mem=$(ps -p "$pd5" -o %mem | sed -n 2p)
+        echo $1,$cpu,$mem >> "APM5_metrics.csv"
+        cpu=$(ps -p "$pd6" -o %cpu | sed -n 2p)
+        mem=$(ps -p "$pd6" -o %mem | sed -n 2p)
+        echo $1,$cpu,$mem >> "APM6_metrics.csv"
 }
- 	trap cleanup SIGINT #If the user clicks CTRL+C (^C) then it  qis accounted for
+
+
+cleanup(){
+	pkill -f -9 APM1
+	pkill -f -9 APM2
+	pkill -f -9 APM3
+	pkill -f -9 APM4
+	pkill -f -9 APM5
+	pkill -f -9 APM6
+	pkill -f -9 "ifstat"
+    exit 0
+}
+
+trap cleanup SIGINT #If the user clicks CTRL+C (^C) then it  qis accounted for
 read -p "Enter the IP address that you would like to use (from the NIC):" IP_ADDRESS
 spawn "$IP_ADDRESS"
 #while loop start for running the script every 5 seconds
@@ -54,6 +76,7 @@ SECONDS=0
 			cleanup
 		fi
 		echo "Collected Data at $duration Seconds" #Every 5 seconds, this is displayed
+        ps_level $SECONDS
 		duration=$SECONDS
 	done
-	
+
